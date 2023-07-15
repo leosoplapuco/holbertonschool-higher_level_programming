6ac09e6ed82d240dd807f9d67498c3842d2427ab
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-07-14 18:43:52 -0700

    2-args 4

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,20 +1,12 @@
 #!/usr/bin/python3
-def print_arg(argv):
-    n = len(argv) - 1
-    if n == 0:
-        print("{:d} argument.".format(n))
-        return
-    else:
-        if n == 1:
-            print("{:d} argument:".format(n))
-        else:
-            print("{:d} arguments:".format(n))
-        i = 1
-        while i <= n:
-            print("{:d}: {:s}".format(i, argv[i]))
-            i += 1
+import sys
 
+argc = len(sys.argv) - 1
+argv = sys.argv[1:]
 
-if __name__ == "__main__":
-    import sys
-    print_arg(sys.argv)
+print(argc, "argument" + ("s" if argc != 1 else "") + ":")
+if argc == 0:
+    print("No arguments.")
+else:
+    for i, arg in enumerate(argv, 1):
+        print(i, ":", arg)
