711ec0e4dd21bd43e44733f900c227c89817ea5c
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-07-18 07:26:53 -0700

    7-update_dictionary 3

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
+my_dict = {'a': 'A', 'b': 'b', 'c': 'c', 'd': 'd', 'e': 'e'}
+print_sorted_dictionary(my_dict)
