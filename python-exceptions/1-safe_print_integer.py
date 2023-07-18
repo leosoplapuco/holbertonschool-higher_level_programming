a13ffb3ca31a30c5a4e3e2828003984f5c103d1e
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-07-18 10:53:13 -0700

    1-safe_print_integer

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
+        if value is None or isinstance(value, set):
+            raise TypeError("Invalid value")
         print("{:d}".format(value))
-    except ValueError:
+    except (ValueError, TypeError):
         return False
     else:
         return True
