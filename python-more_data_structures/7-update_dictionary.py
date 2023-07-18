b272b34abfacf299db6ac2c3464254d848dd1d35
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-07-18 07:25:41 -0700

    7-update_dictionary 2

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-def update_dictionary(my_dict, key, value):
-    my_dict[key] = value
-    return (my_dict
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
