50f9244b81ca1d55945f8a1157c308022e013033
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-07-18 07:48:58 -0700

    11-multiply_list_map 2

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
-def mutiply_list_map(my_list=[], number=0):
-    return (list(map(lambda x: x * number, my_list)))
+def roman_to_int(roman_string):
+    if not roman_string or not isinstance(roman_string, str):
+        return 0
+
+def multiply_list_map(my_list=[], number=0):
+    return list(map(lambda x: x * number, my_list))
+
+print(roman_to_int("XXIV"))
+
+values = [1, 2, 3, 4, 5]
+multiplied_values = multiply_list_map(values, 3)
+print(multiplied_values)
