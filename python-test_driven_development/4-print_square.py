7f3e54eb7dc795a4ed37cad5b7b04ef28fd5676c
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-07-22 15:36:44 -0700

    4-print_square

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""module print_square"""
+
+def print_square(size):
+    """print_square"""
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for _ in range(size):
+        print("#" * size)
