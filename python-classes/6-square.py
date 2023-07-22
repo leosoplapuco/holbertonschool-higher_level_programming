e52ca98501efd8763260b0a9b3ffa9b4e804309b
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-07-22 10:44:31 -0700

    6-square 2

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """class Square"""
 
+
 class Square:
     """class Square"""
-
     def __init__(self, size=0, position=(0, 0)):
         self.size = size
         self.position = position
