0e6489a620be5f68fa72de04da94e825dc71f41f
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-07-22 10:33:28 -0700

    2-square

---
+++ python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""class Square"""
+
+
+class Square:
+    """class Square"""
+    pass
+
+    def __init__(self, size=0):
+        """init method"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
