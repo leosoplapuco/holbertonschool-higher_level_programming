615e64d0841101f687ef7a696d16ec1f607da312
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-07-22 11:14:47 -0700

    0-add_integer 2

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -2,16 +2,14 @@
 """add_integer module"""
 
 
-def add_integer(a, b):
+def add_integer(a, b=98):
     """Return a + b"""
-    if type(a) == float:
-        a = int(a)
-    if type(b) == float:
-        b = int(b)
-
-    if not isinstance(a, int) or isinstance(a, bool):
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if not isinstance(b, int) or isinstance(b, bool):
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
+
+    a = int(a)
+    b = int(b)
 
     return (a + b)
