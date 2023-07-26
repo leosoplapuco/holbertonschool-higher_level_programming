3ff75cee0de97ba673f12b21e51a313e2f74cb6f
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-07-26 13:00:55 -0700

    11-square 2

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -13,6 +13,7 @@ class BaseGeometry:
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
 
+
 class Rectangle(BaseGeometry):
     """class Rectangle"""
     def __init__(self, width, height):
