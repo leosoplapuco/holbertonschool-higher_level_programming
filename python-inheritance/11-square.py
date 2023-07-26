b1c4c05b77a5a776f798123342dbe6190f0096a5
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-07-26 12:59:28 -0700

    11-square

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""module BaseGeometry"""
+
+
+class BaseGeometry:
+    """class BaseGeometry"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+class Rectangle(BaseGeometry):
+    """class Rectangle"""
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+
+class Square(Rectangle):
+    """class Square"""
+    def __init__(self, size):
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def __str__(self):
+        return f"[Square] {self.__size}/{self.__size}"
