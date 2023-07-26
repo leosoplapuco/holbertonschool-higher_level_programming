5fa98f646ac3a7a43a1b477ab56ccc0ec5b07b7b
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-07-26 12:47:20 -0700

    8-rectangle 2

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -15,6 +15,7 @@ class BaseGeometry:
 
 
 class Rectangle(BaseGeometry):
+    """class Rectangle"""
     def __init__(self, width, height):
         self.__width = width
         self.__height = height
