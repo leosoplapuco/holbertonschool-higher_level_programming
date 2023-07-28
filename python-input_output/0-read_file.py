ff3f3ff24292535a7cb5508f6ef4c9140a24b60f
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-07-27 18:59:04 -0700

    0-read_file 3

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""module read_file"""
+
+
 def read_file(filename=""):
+    """function read_file"""
     with open(filename, 'r', encoding='utf-8') as file:
         print(file.read(), end='')
