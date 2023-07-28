c1fdd33e2c92bb059bb8cd9e537ad77dad003d54
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-07-27 18:29:38 -0700

    8-rectangle 9

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -11,7 +11,7 @@ class BaseGeometry:
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError("{} must be greater than 0". format(name))
 
 
 class Rectangle(BaseGeometry):
@@ -21,19 +21,3 @@ class Rectangle(BaseGeometry):
         super().integer_validator("height", height)
         self.__width = width
         self.__height = height
-
-    def area(self):
-        return self.__width * self.__height
-
-    def __str__(self):
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
-
-
-class Square(Rectangle):
-    """class Square"""
-    def __init__(self, size):
-        super().integer_validator("size", size)
-        super().__init__(size, size)
-
-    def __str__(self):
-        return "[Square] {}/{}".format(self._Rectangle__width, self._Rectangle__height)
