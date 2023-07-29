6b908e91d3d44357d71209e403bb716a8886a8c0
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-07-28 19:35:58 -0700

    9-student 3

---
+++ python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""module Student"""
+
+
+class Student:
+    """class Student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        else:
+            json_dict = {}
+            for attr in attrs:
+                if attr in self.__dict__:
+                    json_dict[attr] = self.__dict__[attr]
+            return json_dict
