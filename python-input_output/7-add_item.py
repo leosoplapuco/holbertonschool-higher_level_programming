2d3be88c21030ae1a5568cfc955fdb435809b808
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-07-28 19:15:30 -0700

    7-add_item 5

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
-"""module save_to_json_file"""
+"""function add_item"""
 
 
 import json
+import sys
 
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-def save_to_json_file(my_obj, filename):
-    """function save_to_json_file"""
-    with open(filename, 'w') as f:
-        f.write(json.dumps(my_obj))
+filename = "add_item.json"
+try:
+    py_list = load_from_json_file(filename)
+except FileNotFoundError:
+    py_list = []
+finally:
+    for el in sys.argv[1:]:
+        py_list.append(str(el))
+    save_to_json_file(py_list, filename)
