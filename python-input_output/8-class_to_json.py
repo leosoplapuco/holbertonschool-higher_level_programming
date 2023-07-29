e5cd774e7cbf321a1a3dff4b563bedf538fe7041
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-07-28 19:28:36 -0700

    8-class_to_json

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""module Person"""
+
+class Person:
+    """class Person"""
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+person = Person("John", 30, True)
+json_dict = class_to_json(person)
+print(json_dict)
